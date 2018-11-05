a59b3fbbeb197758c6e69fa49d5b6f0d94972cff
samanthawebster <44168730+samanthawebster@users.noreply.github.com>
2018-11-05 13:07:01 -0500

    Update RunImageSim.m

--- RunImageSim.m
+++ RunImageSim.m
@@ -6,7 +6,7 @@ totalsize = 500
 
 QuantalBrightness = 10;
 den = [10 0] ; % concentration per Beam area of [monomer - dimers]
-agg = [1,2] * QuantalBrightness; % for [monomer - dimers] agg, aggregation???
+agg = [1,2] * QuantalBrightness; % for [monomer - dimers] agg
 radius = 1.5; % the number of pixel per PSF radius
 extra = 2; % this is just to remove the convolution artifact
 
@@ -15,16 +15,14 @@ ima = create_and_convolve(totalsize,den,agg,radius,extra);
 mean(ima(:))
 
 % addEMCCDNoise instead of addPMTNoise
-ima = addEMCCDNoise(ima, {'laserWidth', 20, 'laserShift', [0,0]}, []);
-
-% WN = 0; % the offset value for white noise.
-% [imaWithNoise,NoisyImage] = addPMTnoise(ima,WN); % the value of the slope of PMTNoise can be modified in the function NoisePMT(x,WN)  
+imawithNoise = addEMCCDNoise(ima, {'laserWidth', 20, 'laserShift', [0,0]}, []);
 
 % imshow([ima,imaWithNoise],[]);
+
 ima1 = uint16(ima);
-% imaWithNoise1=uint16(imaWithNoise);
+imaWithNoise1=uint16(imaWithNoise);
+
 savename = input('Please input file name for saving? ','s');
 
 imwrite(ima1,[savename '.tif'],'TIFF')
-
-% imwrite(imaWithNoise1 ,[savename '-with noise.tif'],'TIFF')
+imwrite(imaWithNoise1 ,[savename '-withnoise.tif'],'TIFF')
